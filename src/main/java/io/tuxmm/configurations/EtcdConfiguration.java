eb465f22e4b43c1fa280cd5ad127888be626701e
Marcelo Monteiro (tuxmonteiro) <marcelotmonteiro@gmail.com>
2017-03-06 00:01:11 -0300

    added missing EtcdConfiguration class

---
+++ src/main/java/io/tuxmm/configurations/EtcdConfiguration.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+
+package io.tuxmm.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zalando.boot.etcd.EtcdClient;
+
+@Configuration
+public class EtcdConfiguration {
+
+    @Bean
+    public EtcdClient etcdClient() {
+        return new EtcdClient("http://127.0.0.1:2379");
+    }
+}
